2000e1fd4ab51f0e6b06a9c4cf2a12a4dff3fc0b
Joshua Moir <j.moir.nz@gmail.com>
2021-11-30 13:22:56 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function addition(a, b) {
+  console.log(`the total of these two numbers is : ${a + b}`);
+}
+
+function multiply(a, b) {
+  console.log(`the product of these two numbers is : ${a * b}`);
+}
+
+module.exports = { addition, multiply };
